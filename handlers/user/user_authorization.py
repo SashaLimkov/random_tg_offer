2939eeba73191c89c29983d81cb02517847472e7
SashaLimkov <ghalimov.mansur2@mail.ru>
2022-04-14 23:28:23 +0300

    add delete messages

--- handlers/user/user_authorization.py
+++ handlers/user/user_authorization.py
@@ -17,6 +17,7 @@ from utils.number_validator import is_phone_number_valid
 
 
 async def user_authorization(message: types.Message):
+    await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
     await bot.send_message(chat_id=message.chat.id, text=td.AUTHORIZATION, reply_markup=await ik.user_auth())
 
 
@@ -37,6 +38,10 @@ async def check_phone(message: types.Message, state: FSMContext):
         try:
             authorizted = cur.execute('SELECT state FROM data WHERE id = ?', (message.from_user.id,)).fetchone()[0]
             if authorizted == 1:
+                await bot.delete_message(
+                    chat_id=message.chat.id,
+                    message_id=message.message_id
+                )
                 await bot.send_message(
                     chat_id=message.chat.id,
                     text=td.AGAIN_AUTHORIZATED,
@@ -44,54 +49,62 @@ async def check_phone(message: types.Message, state: FSMContext):
                 )
                 return
         except Exception as e:
-            pass
+            print(e)
         cur.execute('INSERT INTO  data VALUES(?,?,?,?,?)',
-                    (message.from_user.id, phone_number,  'ученик', '0', '0'))
+                    (message.from_user.id, phone_number, 'ученик', '0', '0'))
         base.commit()
         user_type = cur.execute('SELECT role FROM data WHERE id = ?', (message.from_user.id,)).fetchone()[0]
         result = dict(r.json())
-        if result['user'] and not result['is_active']:
-            await bot.send_message(
-                chat_id=message.chat.id,
-                text=td.UNAVALIABLE_AUTH,
-                reply_markup=await ik.user_auth()
-            )
-        if result['user'] and result['is_active']:
-            if user_type == 'ученик':
-                cur.execute('UPDATE data SET state == ? WHERE id == ?',
-                            (1, message.from_user.id))
-                base.commit()
-                await bot.send_message(
-                    chat_id=message.chat.id,
-                    text=td.SUCCESS_LOGIN,
-                    reply_markup=await ik.user_questions()
-                )
-            if user_type == 'куратор':
-                cur.execute('UPDATE data SET state == ? WHERE id == ?',
-                            (1, message.from_user.id))
-                base.commit()
-                await bot.send_message(
-                    chat_id=message.chat.id,
-                    text=td.SUCCESS_LOGIN,
-                    reply_markup=await ik.main_kurator_menu()
-                )
-            if user_type == 'наставник':
-                cur.execute('UPDATE data SET state == ? WHERE id == ?',
-                            (1, message.from_user.id))
-                base.commit()
-                await bot.send_message(
-                    chat_id=message.chat.id,
-                    text=td.SUCCESS_LOGIN,
-                    reply_markup=await ik.main_nastavnik_menu()
-                )
-        if not result['user'] and not result['is_active']:
-            await bot.send_message(
-                chat_id=message.chat.id,
-                text=td.UNAVALIABLE_AUTH_2,
-                reply_markup=ik.user_auth()
-            )
+        await _role_segregated_menu(message, result, user_type)
     else:
         await bot.send_message(
             chat_id=message.chat.id,
             text=td.INVALID_PHONE
         )
+
+
+async def _role_segregated_menu(message: types.Message, result, user_type):
+    if result['user'] and not result['is_active']:
+        await bot.send_message(
+            chat_id=message.chat.id,
+            text=td.UNAVALIABLE_AUTH,
+            reply_markup=await ik.user_auth()
+        )
+    if result['user'] and result['is_active']:
+        if user_type == 'ученик':
+            cur.execute('UPDATE data SET state == ? WHERE id == ?',
+                        (1, message.from_user.id))
+            base.commit()
+            await bot.send_message(
+                chat_id=message.chat.id,
+                text=td.SUCCESS_LOGIN,
+                reply_markup=await ik.user_questions()
+            )
+        if user_type == 'куратор':
+            cur.execute('UPDATE data SET state == ? WHERE id == ?',
+                        (1, message.from_user.id))
+            base.commit()
+            await bot.send_message(
+                chat_id=message.chat.id,
+                text=td.SUCCESS_LOGIN,
+                reply_markup=await ik.main_kurator_menu()
+            )
+        if user_type == 'наставник':
+            cur.execute('UPDATE data SET state == ? WHERE id == ?',
+                        (1, message.from_user.id))
+            base.commit()
+            await bot.send_message(
+                chat_id=message.chat.id,
+                text=td.SUCCESS_LOGIN,
+                reply_markup=await ik.main_nastavnik_menu()
+            )
+    if not result['user'] and not result['is_active']:
+        await bot.send_message(
+            chat_id=message.chat.id,
+            text=td.UNAVALIABLE_AUTH_2,
+            reply_markup=ik.user_auth()
+        )
+    await bot.delete_message(
+        chat_id=message.chat.id,
+        message_id=message.message_id
+    )
