a791e01d1e7ce346c31ec71e399cd7ab99111227
SashaLimkov <ghalimov.mansur2@mail.ru>
2022-04-17 00:41:22 +0300

    start working with django and django orm

---
+++ src/usersupport/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+class TimeBasedModel(models.Model):
+    class Meta:
+        abstract = True
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class TelegramUser(TimeBasedModel):
+    user_id = models.BigIntegerField(
+        primary_key=True, unique=True, verbose_name="UserChatId"
+    )
+    name = models.CharField(max_length=255, verbose_name="UserName")
+
+
+class Role(TelegramUser):
+    telegram_user = models.ForeignKey(TelegramUser, on_delete=models.DO_NOTHING, verbose_name="Пользователь", related_name="User")
+    user_role = models.CharField(max_length=255, verbose_name="Роль")
+
+
+class UserQuestion(TimeBasedModel):
+    user = models.ForeignKey(TelegramUser, on_delete=models.DO_NOTHING, verbose_name="Пользователь")
+    question = models.CharField(max_length=4000, verbose_name="Вопрос")
+    kurators_mes_id = models.BigIntegerField(verbose_name="id вопроса у кураторов")
+    mentors_mes_id = models.BigIntegerField(verbose_name="id вопроса у наставников")
