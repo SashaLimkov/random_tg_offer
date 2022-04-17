4dc977135c385c1bbcd6077b01eb4cd5dfe11c26
SashaLimkov <ghalimov.mansur2@mail.ru>
2022-04-17 18:28:36 +0300

    fix user setup

--- src/bot/handlers/user/setup_role.py
+++ src/bot/handlers/user/setup_role.py
@@ -12,7 +12,7 @@ from usersupport import models
 
 async def setup_user_role(message: types.Message):
     await bot.send_message(
-        chat_id=message.from_user.id,
+        chat_id=message.chat.id,
         text="Введите секретный ключ",
         reply_markup=break_role_keyboard
     )
@@ -23,33 +23,33 @@ async def check_key(message: types.Message, state: FSMContext):
     user_id = message.from_user.id
     secret_key = message.text
     if secret_key == KURATOR_SECRET_KEY:
-        user: models.TelegramUser = await user_db.add_user(user_id=user_id, name=message.from_user.first_name,
-                                                           role="куратор", phone_number=".")
+        user: models.TelegramUser = await user_db.add_user_with_role(user_id=user_id, name=message.from_user.first_name,
+                                                           role="куратор", chat_id = message.chat.id)
         await state.finish()
         await bot.send_message(
-            chat_id=user_id,
+            chat_id=message.chat.id,
             text=f"Вы успешно авторизированы как куратор",
             reply_markup=ReplyKeyboardRemove()
         )
     elif secret_key == NASTAVNIK_SECRET_KEY:
-        user = await user_db.add_user(user_id=user_id, name=message.from_user.first_name,
-                                      role="наставник", phone_nuber=".")
+        user: models.TelegramUser = await user_db.add_user_with_role(user_id=user_id, name=message.from_user.first_name,
+                                                                     role="наставник", chat_id=message.chat.id)
         await state.finish()
         await bot.send_message(
-            chat_id=user_id,
+            chat_id=message.chat.id,
             text=f"Вы успешно авторизированы как наставник",
             reply_markup=ReplyKeyboardRemove()
         )
     elif secret_key == "Отмена":
         await state.finish()
         await bot.send_message(
-            chat_id=user_id,
+            chat_id=message.chat.id,
             text="Действия отменены",
             reply_markup=ReplyKeyboardRemove()
         )
     else:
         await bot.send_message(
-            chat_id=user_id,
+            chat_id=message.chat.id,
             text="Введенный ключ не соответсвует ни одной из ролей, попробуйте заново или нажмите на кнопку",
             reply_markup=break_role_keyboard
         )
